4a92d02d3e16a7702d578dfea34b6b89ef856af5
Pekka Mansikka <peku@pm-netti.com>
2014-10-16 16:26:19 +0300

    Create koschtit_gallery.php

---
+++ admin/language/english/line/koschtit_gallery.php
@@ -0,0 +1,160 @@
+<?php
+// Heading
+$_['heading_title']       = 'Koschtit Gallery'; 
+
+// Texts
+$_['text_about_delete_comment']     = 'You are about to delete this comment:';
+$_['text_add_image'] = 'Add image';
+$_['text_add_watermark'] = 'Add Watermark';
+$_['text_add_imageframe'] = 'Add Image Frame this Image';
+$_['text_found_watermark'] = 'Found in the database, the fact that this figure is already a watermark.';
+$_['text_add_imagefrmae'] = 'Add Image Frame';
+$_['text_found_imageframe'] = 'Found in the database, the fact that this figure is already a imageframe.';
+$_['text_admin_comment'] = 'Admin Comment';
+$_['text_change'] = 'Change Pictures';
+$_['text_exif_data'] = 'Exif Data';
+$_['text_change_filesize'] = 'Resize Image';
+$_['text_change_order'] = 'Change Order';
+$_['text_comment'] = 'Comment';
+$_['text_create_folder'] = 'Create new Folder';
+$_['text_folder_root'] = 'root/';
+$_['text_del'] = 'Delete';
+$_['text_delete'] = 'Delete Image';
+$_['text_delete_comment'] = 'Delete Comment';
+$_['text_delete_images'] = 'Delete images';
+$_['text_delete_vcomment'] = 'Delete';
+$_['text_delete_success']     = 'Success: Picture file is deleted!';
+$_['text_disabled'] = 'Disabled:';
+$_['text_disk_usage'] = 'Disk Usage:';
+$_['text_edit'] = 'Edit';
+$_['text_edit_admin_comment'] = 'Edit Comment';
+$_['text_edit_vcomment'] ='Edit';
+$_['text_edit_comment'] ='Edit viwercomment';
+$_['text_enabled'] = 'Enabled:';
+$_['text_filesize'] = 'File size:';
+$_['text_folders_in'] = 'Folders in "%s":';
+$_['text_gallery_texts'] = 'Installed Gallery texts';
+$_['text_height'] = 'Korkeus';
+$_['text_image_files'] = 'Image files';
+$_['text_load_images'] = 'Upload image';
+$_['text_manage_folder'] = 'Manage Folder';
+$_['text_no_comment'] = 'Not yet comment';
+$_['text_no_comments'] = 'Not yet comments';
+$_['text_page_texts'] = 'Your page text';
+$_['text_pagination']       = 'Showing {start} to {end} of {total} ({pages} Pages)';
+$_['text_pic_height'] = 'Picture height:';
+$_['text_pic_width'] = 'Picture width:';
+$_['text_picture'] = 'Photo:';
+$_['text_selected_gallery'] = 'Selected gallery folder';
+$_['text_settings'] = 'Settings';
+$_['text_success'] = 'Success: Gallery is modified!';
+$_['text_upload_success']     = 'Picture Load Success!';
+$_['text_viewercomments'] = 'Viewer comments';
+$_['text_width'] = 'Leveys';
+$_['text_exif_filename'] = 'Filename';
+$_['text_exif_filedatetime'] = 'File Date Time';
+$_['text_exif_datetimeoriginal']  = 'Date Time Original'; 
+$_['text_exif_filesize']= 'File Size';
+$_['text_exif_filetype'] = 'File Type';
+$_['text_exif_mimetype']   = 'Mime Type';
+$_['text_exif_make']   = 'Make';
+$_['text_exif_model']   = 'Model';
+$_['text_exif_makernote']  = 'Maker Note';
+$_['text_exif_artist']  = 'Artist';
+$_['text_exif_orientation']   = 'Orientation';
+$_['text_exif_xresolution']   = 'X Resolution';
+$_['text_exif_yresolution']   = 'Y Resolution';
+$_['text_exif_resolutionunit']   = 'Resolution Unit';
+$_['text_exif_isospeedratings'] = 'ISO Speed Ratings';
+$_['text_exif_software']   = 'Software';
+$_['text_exif_flashpixversion']   = 'Flash Version';
+$_['text_exif_exposuremode']   = 'Exposure Mode';
+$_['text_exif_whitebalance']   = 'White Balance';
+$_['text_exif_digitalzoomratio']   = 'Digital Zoom Ratio';
+$_['text_error_permission']   = 'Not Permission to Image Direchtories';
+$_['text_photoalbum']   = 'In Photo Directory';
+$_['text_database']   = 'In Database';
+$_['text_edit_folder'] = 'Edit folder';
+
+
+// Entry
+$_['entry_add_admin_comment'] = 'Add admin comment:';
+$_['entry_add_watermark'] = 'Add watermark to images:';
+$_['entry_add_imageframe'] = 'Add Image Frame:';
+$_['entry_album_name'] = 'Album title:';
+$_['entry_directory'] = 'Directory name:';
+$_['entry_folder'] = 'Folder:';
+$_['entry_comm_auto_string'] = 'Auto comment string';
+$_['entry_create_folder'] = 'Create new Folder';
+$_['entry_help_text'] = 'Help Text'; 
+$_['entry_maximum_image_size'] = 'Reduce maximum image size to:';
+$_['entry_nav_next'] = 'Next page';
+$_['entry_nav_back'] = 'Previous page';
+$_['entry_nav_maxi'] = 'Maximize gallery';
+$_['entry_nav_kiv_next'] = 'Next picture';
+$_['entry_nav_kiv_back'] = 'Previous picture';
+$_['entry_nav_kiv_close'] = 'Close';
+$_['entry_nav_gps_coord'] = 'Show on map'; 
+$_['entry_nav_kiv_vcomm'] = 'Add/See viewer comments';
+$_['entry_nav_kiv_download'] = 'Download full resolution picture';
+$_['entry_post'] = 'Post:';
+$_['entry_rotate'] = 'Auto-rotate images:';
+$_['entry_slideshow_start'] = 'Start slideshow';
+$_['entry_slideshow_stop'] = 'Stop slideshow';
+$_['entry_sort_order'] = 'Sort Order:';
+$_['entry_status'] = 'Status:';
+$_['entry_upload'] = 'Upload image:';
+$_['entry_upload_header'] = 'Your browser supports uploading multiple files. Select all files from the "Browse files" upload field'.
+$_['entry_vcomm_lac'] = 'Leave a comment';
+$_['entry_vcomm_name'] = 'Name';
+$_['entry_vcomm_comm'] = 'Comment';
+$_['entry_vcomm_post'] = 'Post comment';
+$_['entry_vcomm_clk'] = 'Click on the image to flip back to the full image.';
+$_['entry_vcomm_ncy'] = 'No comments yet.';
+$_['entry_watermark'] = 'Upload a watermark:';
+$_['entry_writer'] = 'Writer:';
+
+// Button
+$_['button_create_folder'] = ' Create new Folder';
+$_['button_delete_folder'] = 'Delete';
+$_['button_empty_folder'] = 'Empty';
+$_['button_images'] = 'Images';
+$_['button_save_list'] = 'Update images to Database';
+$_['button_update_folder_size'] = 'Update size of folder to database';
+$_['button_update_last_modified'] = 'Update modify time to database';
+
+// Tabs
+$_['tab_comments'] = 'Comments';
+$_['tab_exifdata'] = 'Exif Data';
+$_['tab_general'] = 'General';
+$_['tab_resize'] = 'Resize';
+$_['tab_imageframe'] = 'Image Frame';
+$_['tab_watermark'] = 'Watermark';
+
+// Error
+$_['error_browse_images'] = 'Notice: This directory is not open front. Thumb images is not display, images load can  take a long time.';
+$_['error_file_get_empty'] = 'Filename not found!';
+$_['error_database_list']    = 'Warning: You have moved the images in via FTP? Error Image list in Database!';
+$_['error_image_not_found']    = 'Error: Your Image Not Found!';
+$_['error_image_filename']    = 'Error: Filename is not found!';
+$_['error_change_folder']    = 'Change Folder Error: thumb image is not found! Folder must first open in Front.';
+$_['error_module_not_found']    = 'Error: Module Koschtit Gallery Not Found!';
+$_['error_not_permission']    = 'Error: Not Permission to Image Folder!';
+$_['error_permission']    = 'Warning: You do not have permission to modify Koschtit Gallery!';
+$_['error_system']    = 'Error: Invalid file path to directory System ($system_dir) in file /ki_config/ki_setup.php!';
+$_['error_username']    = 'Error: Invalid username of database ($db_username) in file /ki_config/ki_setup.php!';
+$_['error_password']    = 'Error: Invalid password of database ($db_password) in file /ki_config/ki_setup.php!';
+$_['error_database']    = 'Error: Invalid database name ($db_database) in file /ki_config/ki_setup.php!';
+$_['error_db_server']    = 'Error: Invalid database Server ($db_hostname) in file /ki_config/ki_setup.php!';
+$_['error_db_prefix']    = 'Error: Invalid Prefix of the Database table($db_prefix) in file /ki_config/ki_setup.php!';
+$_['error_folder_name']  = 'Error: Directory name is necessary!';
+$_['error_not_permission_galleries'] = 'Error: Is not permission to Your directory <em>%s</em>. You change permission.';
+$_['error_folder_is_create'] = 'Error: Folder <em>%s</em> is already exists!';
+$_['error_folder_directory_not_found'] = 'Error: Directory <em>%s</em> is not found!';
+
+// Notice
+$_['notice_empty']    = 'Notice: Does not have any folder in Gallery!';
+$_['notice_empty_folder']    = 'Notice: This folder is empty!';
+$_['notice_folder_size'] = 'Notice: Folder size is below strength in Database!';
+$_['notice_help_managefolder']    = 'Update list in managefolder.';
+?>
